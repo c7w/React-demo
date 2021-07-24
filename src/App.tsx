d4367e7b32100dcc61dd4737fd118a700ee7ffdc
c7w <admin@cc7w.cf>
2021-07-24 18:52:25 +0800

    Designed UI; Added Nav, Footer; Modified readme.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Nav from "./Nav"
+import Footer from "./Footer"
+
+
+let [] = [];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <div className="App-background">
+        <div className="App-main">
+          <h1> 及 格 人 数 统 计 </h1>
+
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/Nav.tsx
@@ -0,0 +1,14 @@
+import "./Nav.css"
+
+function Nav() {
+    return (
+        <div className="Nav-Navbar"> 
+            <a href="https://www.github.com/c7w/React-demo-0723" className="Nav-Nav">GitHub</a>
+            &nbsp;&nbsp;&nbsp;&nbsp;
+            <a href="https://cc7w.cf" className="Nav-Nav">Blog</a>
+        </div>
+    );
+
+}
+
+export default Nav;
