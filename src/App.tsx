3ecdd5f74d21de2b798fcd3c2c8031e9775ee5e8
c7w <admin@cc7w.cf>
2021-07-24 20:35:40 +0800

    Updated package.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import React from 'react';
+
 import './App.css';
-import Nav from "./Nav"
-import Footer from "./Footer"
+import Nav from "./Nav";
+import Footer from "./Footer";
 
+import { InputNumber } from 'antd';
 
-let [] = [];
+function onChange(value : number) {
+  console.log('changed', value);
+}
+
 
 function App() {
   return (
@@ -13,7 +17,7 @@ function App() {
       <div className="App-background">
         <div className="App-main">
           <h1> 及 格 人 数 统 计 </h1>
-
+          <InputNumber min={1} max={10} defaultValue={3} onChange={onChange} />
         </div>
       </div>
       <Footer />
